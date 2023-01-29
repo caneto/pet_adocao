0ead14dfd30edf97e3691ad9ebd27ff77d84807b
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-01-29 02:04:30 -0300

    Atualização

--- lib/widgets/notification_box.dart
+++ lib/widgets/notification_box.dart
@@ -1,4 +1,4 @@
-import 'package:badges/badges.dart';
+import 'package:badges/badges.dart' as badge ;
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pet_app/theme/color.dart';
@@ -19,10 +19,10 @@ class NotificationBox extends StatelessWidget {
           color: appBarColor,
           border: Border.all(color: Colors.grey.withOpacity(.3)),
         ),
-        child: notifiedNumber > 0 ? Badge(
+        child: notifiedNumber > 0 ? badge.Badge(
           badgeColor: actionColor,
           padding: EdgeInsets.all(3),
-          position: BadgePosition.topEnd(top: -3, end: -3),
+          position: badge.BadgePosition.topEnd(top: -3, end: -3),
           badgeContent: Text('', style: TextStyle(color: Colors.white),),
           child: SvgPicture.asset("assets/icons/bell.svg", width: 25, height: 25,)
         )
